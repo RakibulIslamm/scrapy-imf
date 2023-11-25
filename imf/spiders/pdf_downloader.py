6bea025efe142506faaa5cc32e5ad7752ed9bd11
root <root@Ubuntu.myguest.virtualbox.org>
2023-11-25 21:07:32 +0600

    pagination (done)

--- imf/spiders/pdf_downloader.py
+++ imf/spiders/pdf_downloader.py
@@ -17,6 +17,7 @@ def should_abort_request(request):
 class PdfDownloaderSpider(scrapy.Spider):
     name = "pdf_downloader"
     num = None
+    page_count = 0
     topics= ['Financial and monetary sector', 'Real sector', 'Fiscal sector', 'External sector', 'Fund operations', 'Cross-sector', 'Economic theory and methods', 'Fund structure and governance', 'Natural resources', 'Environmental policy', 'Environmental sustainability', 'All']
     custom_settings={
         "PLAYWRIGHT_ABORT_REQUEST" : should_abort_request,
@@ -30,6 +31,10 @@ class PdfDownloaderSpider(scrapy.Spider):
     def set_num(cls, selected_num):
         cls.num = selected_num
 
+    @classmethod
+    def set_page_count(cls, page_number):
+        cls.page_count = page_number
+
     def start_requests(self):
         print('\n\n')
         for index, topic in enumerate(self.topics, start=1):
@@ -40,19 +45,17 @@ class PdfDownloaderSpider(scrapy.Spider):
 
 
         if(self.topics[int(self.num)-1] == 'All'):
+            print(f'\n\nSelected Item: {self.topics[int(self.num)-1]}')
             print('Downloading...')
-            print('Please wait a few minutes...')
+            print('Please wait a few minutes...\n')
             yield scrapy.Request(
-                # url='https://www.imf.org/en/Publications/Search#sort=relevancy&numberOfResults=50',
-                url='https://www.imf.org/en/Publications/Search#sort=relevancy',
+                url='https://www.imf.org/en/Publications/Search#sort=relevancy&numberOfResults=50',
                 meta=dict(
-                    playwright_context = "context-0",
-                    playwright_include_page= True,
                     playwright=True,
+                    playwright_context = "context-1",
+                    playwright_include_page= True,
                     playwright_page_methods=[
-                        PageMethod('wait_for_load_state', 'networkidle'),
-                        # PageMethod('wait_for_selector', 'ul.coveo-pager-list'),
-                        # PageMethod('wait_for_timeout', 920000),
+                        PageMethod('wait_for_load_state', 'networkidle')
                     ]
                 ),
                 dont_filter=True,
@@ -60,7 +63,7 @@ class PdfDownloaderSpider(scrapy.Spider):
         elif(int(self.num) <= len(self.topics) and int(self.num) > 0 and self.topics[int(self.num)-1] != 'All'):
             print(f'\n\nSelected Item: {self.topics[int(self.num)-1]}')
             print('Downloading...')
-            print('Please wait a few minutes...')
+            print('Please wait a few minutes...\n')
             yield scrapy.Request(
                 url=f'https://www.imf.org/en/Publications/Search#sort=relevancy&numberOfResults=50&f:topic=[{self.topics[int(self.num)-1]}]',
                 meta=dict(
@@ -68,9 +71,7 @@ class PdfDownloaderSpider(scrapy.Spider):
                     playwright_context= "context-1",
                     playwright_include_page= True,
                     playwright_page_methods=[
-                        PageMethod('wait_for_load_state', 'networkidle'),
-                        # PageMethod('wait_for_selector', 'ul.coveo-pager-list'),
-                        # PageMethod('wait_for_timeout', 920000),
+                        PageMethod('wait_for_load_state', 'networkidle')
                     ]
                 ),
                 dont_filter=True,
@@ -82,9 +83,13 @@ class PdfDownloaderSpider(scrapy.Spider):
     
 
     async def parse(self, response):
-        result_count = response.css('div.coveo-summary-section span')
+        result_count = response.css('div.coveo-summary-section span.CoveoQuerySummary span span.coveo-highlight')
         total_items = result_count[-1]
-        items = total_items.css('.coveo-highlight ::text').get()
+        items = total_items.css('span.coveo-highlight ::text').get()
+
+        page = response.meta["playwright_page"]
+        await page.close()
+        await page.context.close()
 
         result = response.css('div.CoveoResult')
         for item in result:
@@ -95,24 +100,23 @@ class PdfDownloaderSpider(scrapy.Spider):
                     "Title": codecs.decode(title[0:50], 'unicode_escape'),
                     "file_urls": [link]
                 }
-        # url='https://www.imf.org/en/Publications/Search#first=10&sort=relevancy',
 
-
+        self.set_page_count(self.page_count+1)
+        print(f'Page {self.page_count}___(Scraped)')
         # Paginating
         items_count = int(items.replace(',', ''))
         if(self.topics[int(self.num)-1] == 'All'):
             if items_count >= 1000:
-                for i in range(1, 19):
+                for i in range(1, 20):
                     yield scrapy.Request(
                         url=f'https://www.imf.org/en/Publications/Search#first={i*50}&sort=relevancy&numberOfResults=50',
+                        callback= self.parse_rest,
                         meta=dict(
                             playwright_context = f"context-{(i+1)}",
                             playwright_include_page= True,
                             playwright=True,
                             playwright_page_methods=[
                                 PageMethod('wait_for_load_state', 'networkidle'),
-                                # PageMethod('wait_for_selector', 'ul.coveo-pager-list'),
-                                # PageMethod('wait_for_timeout', 920000),
                             ]
                         ),
                         dont_filter=True,
@@ -121,28 +125,55 @@ class PdfDownloaderSpider(scrapy.Spider):
                 for i in range(1, math.ceil(items_count/50)):
                     yield scrapy.Request(
                         url=f'https://www.imf.org/en/Publications/Search#first={i*50}&sort=relevancy&numberOfResults=50',
+                        callback= self.parse_rest,
                         meta=dict(
                             playwright_context = f"context-{(i+1)}",
                             playwright_include_page= True,
                             playwright=True,
                             playwright_page_methods=[
                                 PageMethod('wait_for_load_state', 'networkidle'),
-                                # PageMethod('wait_for_selector', 'ul.coveo-pager-list'),
-                                # PageMethod('wait_for_timeout', 920000),
                             ]
                         ),
                         dont_filter=True,
                     )
-        elif(int(self.num) <= len(self.topics) and int(self.num) > 0 and self.topics[int(self.num)-1] != 'All'):
-            pass
+        elif(int(self.num) < len(self.topics) and int(self.num) > 0 and self.topics[int(self.num)-1] != 'All'):
+            if items_count >= 1000:
+                for i in range(1, 20):
+                    yield scrapy.Request(
+                        url=f'https://www.imf.org/en/Publications/Search#first={i*50}&sort=relevancy&numberOfResults=50&f:topic=[{self.topics[int(self.num)-1]}]',
+                        callback= self.parse_rest,
+                        meta=dict(
+                            playwright_context = f"context-{(i+1)}",
+                            playwright_include_page= True,
+                            playwright=True,
+                            playwright_page_methods=[
+                                PageMethod('wait_for_load_state', 'networkidle'),
+                            ]
+                        ),
+                        dont_filter=True,
+                    )
+            elif(items_count < 1000 and items_count > 0):
+                for i in range(1, math.ceil(items_count/50)):
+                    yield scrapy.Request(
+                        url=f'https://www.imf.org/en/Publications/Search#first={i*50}&sort=relevancy&numberOfResults=50&f:topic=[{self.topics[int(self.num)-1]}]',
+                        callback= self.parse_rest,
+                        meta=dict(
+                            playwright_context = f"context-{(i+1)}",
+                            playwright_include_page= True,
+                            playwright=True,
+                            playwright_page_methods=[
+                                PageMethod('wait_for_load_state', 'networkidle'),
+                            ]
+                        ),
+                        dont_filter=True,
+                    )
         else:
             return
 
-        print("\nPaginating...\n")
+        print('Paginating...')
 
     async def parse_rest(self, response):
         page = response.meta["playwright_page"]
-        # context_name = response.meta["playwright_context"]
         result = response.css('div.CoveoResult')
         await page.close()
         await page.context.close()
@@ -154,6 +185,8 @@ class PdfDownloaderSpider(scrapy.Spider):
                     "Title": codecs.decode(title[0:50], 'unicode_escape'),
                     "file_urls": [link]
                 }
+        self.set_page_count(self.page_count+1)
+        print(f'Page {self.page_count}___(Scraped)')
 
 
 
